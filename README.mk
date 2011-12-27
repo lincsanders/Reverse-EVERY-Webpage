640ff5619d6b48ca2ddaadc02ec1df8f066159a3
Lincoln Sanderson <lincoln.sanderson@paperton.com>
2011-12-27 15:57:43 +0100

    Adding README for basic instructions

---
+++ README.mk
@@ -0,0 +1,4 @@
+ Installation
+ ====================
+
+ Simply download flip.user.js and drag and drop onto your victims installation of Chrome. Requires the latest version, but I would be surprised if that is not already installed.
